52c0b40641592338fea43c157ddc2a3e8517c2e1
Joyce Ren <joyce.y.ren@gmail.com>
2018-01-22 17:29:40 -0800

    new design with firebase

---
+++ fire/setup.js
@@ -0,0 +1,14 @@
+module.exports = firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyCYx6jRr5HvhBnSSvIti-dztkf6obUR-jc",
+  "databaseURL": "https://zero-f-for.firebaseio.com",
+  "storageBucket": "zero-f-for.appspot.com",
+  "authDomain": "zero-f-for.firebaseapp.com",
+  "messagingSenderId": "1079584658631",
+  "projectId": "zero-f-for"
+});
+
